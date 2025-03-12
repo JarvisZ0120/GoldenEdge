1fd4daaba355e89712ec87a501d92910a5d2fa5a
Damon <damonjiangwork@gmail.com>
2025-03-11 23:39:18 -0600

    print changed to Chinese, added logger as an arguement

--- MetaTrader/mt5_connection.py
+++ MetaTrader/mt5_connection.py
@@ -1,11 +1,14 @@
 import MetaTrader5 as mt5
+from Others.log_manager import LogManager
+
 
 class MT5Connection:
-    def __init__(self, account, password, server):
+    def __init__(self, account, password, server, logger):
         self.account = account
         self.password = password
         self.server = server
         self.connected = False
+        self.logger = logger
 
     def connect(self):
         """
@@ -15,19 +18,25 @@ class MT5Connection:
             boolean: True 如果登录上， 否则False 
         """
         if not mt5.initialize():
-            print("⚠️ MT5 初始化失败！")
+            print("\n⚠️ MT5 初始化失败！")
+            self.logger.info("")
+            self.logger.error("⚠️ Failed to initialize MT5! ")
             quit()
             return False
 
         authorized = mt5.login(self.account, password=self.password, server=self.server)
         if not authorized:
-            print("⚠️ MT5 登录失败！错误代码：", mt5.last_error())
+            print("\n⚠️ MT5 登录失败！错误代码：", mt5.last_error())
+            self.logger.info("")
+            self.logger.error("⚠️ Failed to log in MT5! error code: ", mt5.last_error())
             mt5.shutdown()
             quit()
             return False
 
         self.connected = True
-        print("✅ MT5 连接成功！")
+        print("\n✅ MT5 连接成功！")
+        self.logger.info("")
+        self.logger.info("✅ Successfully connected to MT5! ")
         return True
 
     def disconnect(self):
@@ -36,7 +45,9 @@ class MT5Connection:
         """
         mt5.shutdown()
         self.connected = False
-        print("⚠️ MT5 断开连接！")
+        print("\n⚠️ MT5 断开连接！")
+        self.logger.info("")
+        self.logger.error("⚠️ Successfully disconnected to MT5! ")
 
     def is_connected(self):
         """
@@ -50,16 +61,21 @@ class MT5Connection:
     def get_mt5(self):
         return mt5
 
+# 示例用法
 if __name__ == "__main__":
-     # 配置登录信息
+    # 配置looger
+    log_mamanger = LogManager()
+    logger = log_mamanger.get_logger()
+
+    # 配置登录信息
     account = 5033993521
     password = "G!I0FwJn"
     server = "MetaQuotes-Demo"
 
     # 创建 MT5 连接实例
-    mt5_connection = MT5Connection(account, password, server)
+    mt5_connection = MT5Connection(account, password, server, logger)
 
     # 连接到 MT5
     mt5_connection.connect()
-    print(f"已经连接：{mt5_connection.is_connected()}")
+    print(f"\n已经连接：{mt5_connection.is_connected()}")
     # mt5_connection.disconnect()
