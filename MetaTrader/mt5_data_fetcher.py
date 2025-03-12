bd5a95826b5c8c1abaf0f3c1f31f41bbb481b3ef
Damon <damonjiangwork@gmail.com>
2025-03-11 23:39:51 -0600

    print changed to Chinese, added logger as an arguement

--- MetaTrader/mt5_data_fetcher.py
+++ MetaTrader/mt5_data_fetcher.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from MetaTrader.mt5_connection import MT5Connection
+from Others.log_manager import LogManager
+
 
 class MT5DataFetcher:
-    def __init__(self, mt5_connection):
+    def __init__(self, mt5_connection, logger):
         self.mt5_connection = mt5_connection
+        self.logger = logger
 
     def get_timeframe(self,timrframe):
         """
         获取 MetaTrader5 相对应的 TIMEFRAME
         """
-
         mt5 = self.mt5_connection.get_mt5()
         switch = {
             "M1": mt5.TIMEFRAME_M1,
@@ -30,16 +32,21 @@ class MT5DataFetcher:
         """
         # 确保连接已建立
         if not self.mt5_connection.is_connected():
-            print("⚠️ 请先连接到 MetaTrader 5! ")
+            print("\n⚠️ 请先连接到 MetaTrader 5! ")
+            self.logger.info("")
+            self.logger.error("⚠️ Failed to fetch OHLCV! (MT5 is not connected.) ")
             return pd.DataFrame()
 
         mt5 = self.mt5_connection.get_mt5()
         timeframe = self.get_timeframe(timeframe) 
+
         # 获取最近 1000 根 K 线
         rates = mt5.copy_rates_from_pos(symbol, timeframe, 0, 1000)
         
         if rates is None or len(rates) == 0:
-            print("⚠️ 无法获取数据! ")
+            print("\n⚠️ 无法获取数据! ")
+            self.logger.info("")
+            self.logger.info("⚠️ Failed to fetch OHLCV! (Rates from MetaTrader5 is untrackable.) ")
             return pd.DataFrame()
         
         # 转换为 DataFrame 格式
@@ -49,25 +56,27 @@ class MT5DataFetcher:
 
 # 示例用法：
 if __name__ == "__main__":
+    # 配置 logger 
+    log_manager = LogManager()
+    logger = log_manager.get_logger()
+
     # 配置登录信息和数据
     account = 5033993521
     password = "G!I0FwJn"
     server = "MetaQuotes-Demo"
 
-    mt5_connection = MT5Connection(account, password, server)
+    mt5_connection = MT5Connection(account, password, server, logger)
     mt5_connection.connect()
 
     # 创建 MT5DataFetcher 实例
-    mt5_data_fetcher = MT5DataFetcher(mt5_connection)
+    mt5_data_fetcher = MT5DataFetcher(mt5_connection, logger)
     
     # 获取 OHLCV 数据
     ohlcv_data = mt5_data_fetcher.fetch_ohlcv("XAUUSD", "M1")
     
     # 显示数据
     if not ohlcv_data.empty:
-        print(ohlcv_data.tail(5))
+        print(f"\n数据：{ohlcv_data.tail(5)}")
     else:
-        print("No data fetched.")
+        print("\n⚠️ 获取数据失败！")
     
-    # 断开连接
-    mt5_connection.disconnect()
