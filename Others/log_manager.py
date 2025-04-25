e413ff839c6b6443dd7077739ab4ec5d11f7cc87
Damon <damonjiangwork@gmail.com>
2025-04-25 15:30:08 -0600

    refined log to fit both mac and windows

--- Others/log_manager.py
+++ Others/log_manager.py
@@ -2,24 +2,27 @@ import logging
 import os
 from datetime import datetime
 
+
 class LogManager:
     def __init__(self, prefix="app_M1", log_dir="Logs"):
         self.log_dir = log_dir
         os.makedirs(self.log_dir, exist_ok=True)
 
-        self.date_time_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-        self.log_filename = os.path.join(self.log_dir, f"{prefix}_{self.date_time_str}.log")
+        self.date_time_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.log_filename = os.path.join(
+            self.log_dir, f"{prefix}_{self.date_time_str}.log"
+        )
         self._configure_logging()
-    
+
     def _configure_logging(self):
         logging.basicConfig(
             level=logging.DEBUG,
-            format='%(asctime)s - %(levelname)s - %(message)s',
+            format="%(asctime)s - %(levelname)s - %(message)s",
             filename=self.log_filename,
-            filemode='a',
-            encoding="utf-8"  
+            filemode="a",
+            encoding="utf-8",
         )
-    
+
     @staticmethod
     def get_logger():
         return logging.getLogger()
